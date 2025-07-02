5fc838969a2688cf58352aceb7396284bed88b17
Keilin Balbuena <keypolanco789@gmail.com>
2025-07-02 11:48:57 -1200

    rutas controllers

--- Procomsumidor/routes/api.php
+++ Procomsumidor/routes/api.php
@@ -2,8 +2,41 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\BranchController;
 
-Route::middleware('api')->group(function () {
-    Route::apiResource('branches', BranchController::class);
+use App\Http\Controllers\API\BranchController;
+use App\Http\Controllers\API\ClaimController;
+use App\Http\Controllers\API\ClaimFromController;
+use App\Http\Controllers\API\ClaimStatusController;
+use App\Http\Controllers\API\ClaimUpdateController;
+use App\Http\Controllers\API\ConsumerController;
+use App\Http\Controllers\API\DepartmentController;
+use App\Http\Controllers\API\EntryWayController;
+use App\Http\Controllers\API\MotiveController;
+use App\Http\Controllers\API\MunicipioController;
+use App\Http\Controllers\API\OfficeController;
+use App\Http\Controllers\API\ProductTypeController;
+use App\Http\Controllers\API\ProfileController;
+use App\Http\Controllers\API\ProviderController;
+use App\Http\Controllers\API\SectorController;
+use App\Http\Controllers\API\UserController;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResources([
+        'branches' => BranchController::class,
+        'claims' => ClaimController::class,
+        'claims-from' => ClaimFromController::class,
+        'claim-statuses' => ClaimStatusController::class,
+        'claim-updates' => ClaimUpdateController::class,
+        'consumers' => ConsumerController::class,
+        'departments' => DepartmentController::class,
+        'entry-ways' => EntryWayController::class,
+        'motives' => MotiveController::class,
+        'municipios' => MunicipioController::class,
+        'offices' => OfficeController::class,
+        'product-types' => ProductTypeController::class,
+        'profiles' => ProfileController::class,
+        'providers' => ProviderController::class,
+        'sectors' => SectorController::class,
+        'users' => UserController::class,
+    ]);
 });
