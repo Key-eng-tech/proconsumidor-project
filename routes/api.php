6c176861361e754d089f1e636e3629fcf8cec307
MoisésB27 <moisesdavidbatista@gmail.com>
2025-07-03 10:58:14 -0400

    Automatizar controladores

--- routes/api.php
+++ routes/api.php
@@ -1,43 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
-use App\Http\Controllers\API\ProvinceController;
-use App\Http\Controllers\API\BranchController;
-use App\Http\Controllers\API\ClaimController;
-use App\Http\Controllers\API\ClaimFromController;
-use App\Http\Controllers\API\ClaimStatusController;
-use App\Http\Controllers\API\ClaimUpdateController;
-use App\Http\Controllers\API\ConsumerController;
-use App\Http\Controllers\API\DepartmentController;
-use App\Http\Controllers\API\EntryWayController;
-use App\Http\Controllers\API\MotiveController;
-use App\Http\Controllers\API\MunicipioController;
-use App\Http\Controllers\API\OfficeController;
-use App\Http\Controllers\API\ProductTypeController;
-use App\Http\Controllers\API\ProfileController;
-use App\Http\Controllers\API\ProviderController;
-use App\Http\Controllers\API\SectorController;
-use App\Http\Controllers\API\UserController;
 
-Route::apiResource("provinces", ProvinceController::class);
-Route::apiResource('branches', BranchController::class);
-Route::apiResource('claims', ClaimController::class);
-Route::apiResource('claims-from', ClaimFromController::class);
-Route::apiResource('claim-statuses', ClaimStatusController::class);
-Route::apiResource('claim-updates', ClaimUpdateController::class);
-Route::apiResource('consumers', ConsumerController::class);
-Route::apiResource('departments', DepartmentController::class);
-Route::apiResource('entry-ways', EntryWayController::class);
-Route::apiResource('motives', MotiveController::class);
-Route::apiResource('municipios', MunicipioController::class);
-Route::apiResource('offices', OfficeController::class);
-Route::apiResource('product-types', ProductTypeController::class);
-Route::apiResource('profiles', ProfileController::class);
-Route::apiResource('providers', ProviderController::class);
-Route::apiResource('sectors', SectorController::class);
-Route::apiResource('users', UserController::class);
+$models = ['User', 'Product', 'Sector', 'Province', 'Provider', 'Profile', 'ProductType', 'Office',
+           'Municipio', 'Motive', 'EntryWay', 'Department', 'Consumer', 'ClaimUpdate', 'ClaimStatus', 'ClaimFrom', 'Claim', 'Branch']; // Lista de modelos 
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () use 
+($models) {  
+
+foreach ($models as $model) {
+    $controller = "App\\Http\\Controllers\API\{$model}Controller";
+    if (class_exists($controller)){
+        $routeName = strtolower(Str::plural($model));
+        Route::resource($routeName, $controller)->except(['create', 'edit']);
+
+    } else {
+
+         logger("Controlador {$controller} no encontrado.");
+    }
+
+    // Verifica si el controlador existe antes de definir una ruta
+    Route::resource($routeName, $controller);
+}
+
+});
